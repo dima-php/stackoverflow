c183962cd46c185c16de8a3679a0c2306601a74b
dima-php <dj.motor88@gmail.com>
2020-08-05 20:59:06 +0300

    writing answers and validate

---
+++ app/Http/Controllers/AnswerController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Answer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AnswerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'body' => 'required',
+        ]);
+
+
+        $answer = new Answer;
+        $answer->user_id = Auth::id();
+        $answer->question_id = $request->question_id;
+        $answer->votes_count = $request->votes_count;
+        $answer->body = $request->body;
+        $answer->save();
+        return redirect()->route('questions.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/pages/questions/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+@section('content')
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-lg-9 m-auto ">
+                <div class="dk-question-form">
+                <form method="post" action="{{route('questions.store')}}">
+                    @csrf
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Title</label>
+                        <input type="text" name="title" class="form-control" placeholder="Title">
+
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">body</label>
+                        <textarea name="body"  class="form-control" placeholder="Text"></textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+
+
+@endsection
